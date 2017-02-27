2dac360da095d7e20d42522380d98f7248d946b7
Hafeez <feeziman007@hotmail.com>
2017-02-27 22:39:28 +1100

    adding node installation to the docker build

---
+++ node.js-app/foo.js
@@ -0,0 +1,19 @@
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+ Author: Hafeez Syed
+ File: foo.js
+ Created on 26/2/17 11:33 PM
+ Project: watchmecode-docker
+ Description: < DESCRIPTION HERE >
+
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+module.exports = function(words) {
+	console.log('HELLO ' + words);
+};
---
+++ node.js-app/index.js
@@ -0,0 +1,24 @@
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+ Author: Hafeez Syed
+ File: index.js
+ Created on 26/2/17 11:02 PM
+ Project: watchmecode-docker
+ Description: < DESCRIPTION HERE >
+
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+'use strict';
+
+var foo = require( './foo' );
+var lp = require( 'left-pad' );
+
+var stuff = lp("WORLD!", 10);
+
+foo(stuff);
